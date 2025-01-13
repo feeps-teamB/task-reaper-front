f4f6d99fb5851969d72a8f9199ac029ff8088e11
hitugihane <hituzihane01@gmail.com>
2025-01-13 16:33:27 +0900

    add: タスク詳細の表示機能のみ実装

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>リンク置き場</p>
+    <nuxt-link to="/scheduleDetailView/1" class="schedule-link">スケジュール1詳細</nuxt-link>
+    <nuxt-link to="/scheduleDetailView/2" class="schedule-link">スケジュール2詳細</nuxt-link>
+  </div>
+</template>
