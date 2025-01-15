7d826feb338a9fea63dd396e7b9a4493c4759819
hermx09 <sui.nagayama@gmail.com>
2025-01-16 00:58:26 +0900

    表示更新

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt'
+  ],
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      ]
+    }
+  }
 })
--- app/pages/mainPage.vue
+++ app/pages/mainPage.vue
@@ -1,81 +1,45 @@
 <template>
+  <div class = "header">
+    <Header />
+  </div>
   <div class = "main">
     <div class = "selectContainer">
-      <div class = "monthGrid">
       <SelectMonth />
-      </div>
       <SelectButton />
     </div>
-    <div class = "calendar">
-      <FullCalendar :options="calendarOptions" />
-    </div>
+    <div>
+      <FullCalendarComp />
+    </div>  
   </div>
 </template>
 
 <script>
-  import FullCalendar from '@fullcalendar/vue3'
-  import dayGridPlugin from '@fullcalendar/daygrid'
-  import timeGridPlugin from '@fullcalendar/timegrid'
+  import FullCalendarComp from '@/components/fullCalendarComp'
   import SelectMonth from '@/components/selectMonth'
   import SelectButton from '@/components/selectButton'
+  import Header from '@/components/header'
   
   export default {
     name: 'mainPage',
     components: {
-      FullCalendar,
+      FullCalendarComp,
       SelectMonth,
-      SelectButton
-    },
-    data() {
-      return {
-        calendarOptions: {
-          plugins: [dayGridPlugin, timeGridPlugin],
-          initialView: 'dayGridMonth',
-          events: [
-          { title: 'Event 1', start: '2024-12-01', end: '2024-12-01'},
-          { title: 'Event 2', date: '2024-12-29' }
-          ]
-        },
-        calendar: null
-      }
-    },
-    mounted(){
-      this.$nextTick(() => {
-        this.calendar = this.$refs.fullcalendar.getApi();
-        getYear(this.calendar);
-      });
-    },
-    methods: {
-      getYear(calendar){
-        if (calendar) {
-        const currentDate = calendar.getDate(); // 現在の中央日付を取得
-        const year = currentDate.getFullYear(); // 年を取得
-        console.log('現在の年:', year);
-        return year;
-        }else{
-          console.log("カレンダーが初期化されていません");
-        }
-      }
+      SelectButton,
+      Header
     }
   }
 </script>
 
 <style>
-
-.monthGrid{
-    width: 20%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-auto-rows: 100px;
-}
-
-.monthGrid > div{
-    font-size: 1rem;
+.header{
+  background-color: #E3EBF6;
 }
 
 .main{
   display: flex;
   justify-content: space-between;
+  padding: 6vh;
+  background-color: #7FA08B;
 }
 
 .selectContainer{
@@ -84,16 +48,6 @@
   flex-direction: column;
 }
 
-.calendar{
-  width: 40%;
-  height: 40%;
-  position: absolute;
-  margin: 0 auto;
-  top: 250px;
-  right: 500px;
-}
 
-.fc{
-  font-size: 0.8rem;
-}
 </style>
+
