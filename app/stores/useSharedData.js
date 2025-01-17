57fe29fcb2d7679ebdaae5e62dfe5212f1ce454a
hermx09 <sui.nagayama@gmail.com>
2025-01-18 01:15:45 +0900

    createCalendar

--- app/stores/useSharedData.js
+++ app/stores/useSharedData.js
@@ -3,11 +3,15 @@ import { defineStore } from 'pinia';
 export const useSharedData = defineStore('sharedData', {
     state: () => ({
       sharedYear: 2025,
+      calendar: null
     }),
     actions: {
       updateData(newData) {
         this.sharedYear = newData;
       },
+      setCalendar(calendar){
+        this.calendar = calendar;
+      }
     },
   });
   
