701a21539d4baa3b0bd4a7c9905174841e56c09b
Shota0528 <shota05282002@gmail.com>
2025-01-18 01:54:34 +0900

    fix: piniaの修正

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,15 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt'
-  ],
-  app: {
-    head: {
-      link: [
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
-      ]
-    }
-  }
-})
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   modules: [
+//     '@pinia/nuxt'
+//   ],
+
+//   pinia: {
+//     autoImports: [
+//       'defineStore', // 使用する場合に便利
+//     ],
+//   },
+
+//   app: {
+//     head: {
+//       link: [
+//         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+//       ]
+//     }
+//   },
+
+//   compatibilityDate: '2025-01-18',
+// })
