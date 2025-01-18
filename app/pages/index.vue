ffcc309314c52075396220fb8683889c6bf7f898
taka-yu-ki <taka7_zegs@icloud.com>
2025-01-18 12:16:55 +0900

    リンクの修正

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,31 +3,42 @@
     <header class="header">
       <nav class="navigation">
         <img
-        loading="lazy"
-        src="../static/images/icon.png"
-        class="brand"
-        alt="Task Reaper Logo"
+          loading="lazy"
+          src="../static/images/icon.png"
+          class="brand"
+          alt="Task Reaper Logo"
         />
-        
+
         <div class="nav-links">
           <a href="#learn" class="nav-link">Learn More</a>
           <a href="#contact" class="nav-link">Contact</a>
         </div>
         <div class="auth-links">
-          <a href="/login" class="auth-link login-link">Login</a>
-          <a href="/signup" class="auth-link signup-link">Signup</a>
+          <a href="/mainPage" class="auth-link login-link">Login</a>
+          <a href="/mainPage" class="auth-link signup-link">Signup</a>
         </div>
       </nav>
     </header>
     <section class="hero-section">
       <img
-      loading="lazy"
-      src="https://cdn.builder.io/api/v1/image/assets/TEMP/f0880341d7ce3efe2d49721da222751f2372735e812c2a98acc566e9587bbb8b?placeholderIfAbsent=true&apiKey=37f273673bad4412bbe7062a6725c33c"
-      class="hero-image"
-      alt="Task Reaper Hero Banner"
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/f0880341d7ce3efe2d49721da222751f2372735e812c2a98acc566e9587bbb8b?placeholderIfAbsent=true&apiKey=37f273673bad4412bbe7062a6725c33c"
+        class="hero-image"
+        alt="Task Reaper Hero Banner"
       />
     </section>
-    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1); position: absolute; top: 380px; left: 830px;" width="510" height="380" src="https://embed.figma.com/proto/PjzjpL38bKyQR1XsqGzepq/TASK-REAPER?node-id=25-2259&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=25%3A2259&show-proto-sidebar=0&embed-host=share" allowfullscreen></iframe>
+    <iframe
+      style="
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        position: absolute;
+        top: 380px;
+        left: 830px;
+      "
+      width="510"
+      height="380"
+      src="https://embed.figma.com/proto/PjzjpL38bKyQR1XsqGzepq/TASK-REAPER?node-id=25-2259&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=25%3A2259&show-proto-sidebar=0&embed-host=share"
+      allowfullscreen
+    ></iframe>
     <!-- <div>
       <p>リンク置き場</p>
       <nuxt-link to="/mainPage" class="schedule-link">メイン</nuxt-link>
@@ -66,7 +77,7 @@
 }
 
 .header {
-  background: #7FA08B;
+  background: #7fa08b;
   width: 100%;
   padding: 20px 23px;
 }
@@ -77,7 +88,7 @@
 }
 
 .brand {
-  color: #2F5E39;
+  color: #2f5e39;
   text-align: center;
   font: 700 28px Inter, sans-serif;
 }
@@ -99,7 +110,7 @@ font: inherit;
 .nav-links {
   display: flex;
   align-self: stretch;
-  color: #E3EBF6;
+  color: #e3ebf6;
   text-align: center;
   margin: auto 0;
   font: 700 28px Inter, sans-serif;
@@ -130,11 +141,11 @@ font: inherit;
 }
 
 .login-link {
-  color: #2F5E39;
+  color: #2f5e39;
 }
 
 .signup-link {
-  color: #E3EBF6;
+  color: #e3ebf6;
 }
 
 .hero-section {
@@ -150,8 +161,7 @@ font: inherit;
 .hero-image {
   position: absolute;
   inset: 0;
-  width: 100%;  /* 画像の幅を調整 */
+  width: 100%; /* 画像の幅を調整 */
   object-fit: cover;
 }
 </style>
-
